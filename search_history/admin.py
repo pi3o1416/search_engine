5c3588516e6b3b8968dc23c2b71ddd2a7f656515
monir hossain <mmonir.m.29@gmail.com>
2022-03-03 19:46:43 +0600

    Update Admin site for SearchHistory

--- search_history/admin.py
+++ search_history/admin.py
@@ -8,3 +8,9 @@ from .models import SearchHistory
 @admin.register(SearchHistory)
 class SearchHistoryAdmin(admin.ModelAdmin):
     list_display = ('user', 'search_text', 'search_time')
+    list_filter = ('user', 'search_time')
+    search_fields = ('search_text', 'user__username')
+    ordering = ('user',)
+
+
+
