9f410eea217d6ef396b718bd17bb4f855eae2ec1
monir hossain <mmonir.m.29@gmail.com>
2022-03-03 19:28:36 +0600

    Add SearchHistory table/model

--- search_history/admin.py
+++ search_history/admin.py
@@ -1,3 +1,10 @@
+
+
 from django.contrib import admin
+from .models import SearchHistory
 
 # Register your models here.
+
+@admin.register(SearchHistory)
+class SearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'search_text', 'search_time')
--- search_history/models.py
+++ search_history/models.py
@@ -1,3 +1,33 @@
+
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 # Create your models here.
+
+
+class SearchHistory(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        related_name='user_searched',
+        on_delete=models.CASCADE,
+    )
+
+    search_text = models.CharField(
+        verbose_name=_('User search text'),
+        max_length=200,
+        blank=False,
+    )
+
+    search_time = models.DateTimeField(
+        verbose_name=_('Search Time'),
+    )
+
+    def __str__(self):
+        return self.search_text
+
+
+
+
+
