051a616a77cb8aeded3ae9a5a1b4fc6a9848f6a2
monir hossain <mmonir.m.29@gmail.com>
2022-03-03 18:53:07 +0600

    Add CustomUser in django Administrator

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
+
