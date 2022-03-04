e3b6146ec16e9c4a9715d958555603669f2272aa
monir hossain <mmonir.m.29@gmail.com>
2022-03-04 16:05:54 +0600

    Update list view pagination with ajax

--- search_history/views.py
+++ search_history/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 from .models import SearchHistory
 
 
@@ -13,12 +14,20 @@ class SearchHistoryList(View):
     def get(self, request):
         paginator = Paginator(self.query_set, 3)
         page = request.GET.get('page')
+        print(page)
         try:
+            print('here')
             searched_items = paginator.page(page)
         except PageNotAnInteger:
+            print('page not int')
             searched_items = paginator.page(1)
         except EmptyPage:
+            print('empty page')
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return HttpResponse('')
             searched_items = paginator.page(paginator.num_pages)
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return render(request, 'searchHistory/content_list.html', {'searched_items': searched_items})
         return render(request, self.template_name, {'searched_items': searched_items})
 
 
@@ -32,8 +41,6 @@ class SearchHistoryDetail(View):
     def get(self, request, pk):
         searched_item = SearchHistory.objects.get(pk=pk)
         return render(request,  self.template_name, {'searched_item': searched_item})
-
-
 
 
 
