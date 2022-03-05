b9864d3f257386f5a7821cf7ce5f9ebbaa687601
monir hossain <mmonir.m.29@gmail.com>
2022-03-05 22:48:27 +0600

    add static root in settings

--- search_engine/settings.py
+++ search_engine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Custom Settings
 AUTH_USER_MODEL = 'account.CustomUser'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 
 
