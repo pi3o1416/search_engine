77adebf8659ed71b6c720bc1ea809455ad6fe5f6
monir hossain <mmonir.m.29@gmail.com>
2022-03-05 15:33:18 +0600

    Add test for models and views

---
+++ search_history/tests/test_models.py
@@ -0,0 +1,80 @@
+
+from django.test import TestCase
+from search_history.models import SearchHistory, KeywordOccurences
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+import json
+
+
+User = get_user_model()
+
+
+class TestSearchHistory(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shifat',
+            email='shifat@gmail.com',
+            password='shifat123',
+        )
+        self.history = SearchHistory.objects.create(
+            user=self.user,
+            search_text='ukraine current situation',
+            search_time=timezone.now(),
+            search_result=json.dumps({'hello': 'world'})
+        )
+
+    def test_search_text_label(self):
+        self.assertEqual(self.history._meta.get_field('search_text').verbose_name, 'Search text')
+
+    def test_search_text_max_length(self):
+        self.assertEqual(self.history._meta.get_field('search_text').max_length, 200)
+
+    def test_search_text_black_state(self):
+        self.assertEqual(self.history._meta.get_field('search_text').blank, False)
+
+    def test_search_time_label(self):
+        self.assertEqual(self.history._meta.get_field('search_time').verbose_name, 'Search time')
+
+    def test_search_result_label(self):
+        self.assertEqual(self.history._meta.get_field('search_result').verbose_name, 'Search Result')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.history.get_absolute_url(), '/history/detail/{}'.format(self.history.pk))
+
+
+    class TestKeywordsOccurence(TestCase):
+        def setUp(self):
+            self.user = User.objects.create_user(
+                username='shifat',
+                email='shifat@gmail.com',
+                password='shifat123',
+            )
+            self.history = SearchHistory.objects.create(
+                user=self.user,
+                search_text='ukraine current situation',
+                search_time=timezone.now(),
+                search_result=json.dumps({'hello': 'world'})
+            )
+
+        def test_autometic_generation(self):
+            date = timezone.now().date()
+            keywords = KeywordOccurences.objects.get(date=date)
+            self.assertTrue(keywords)
+
+        def test_date_lable(self):
+            date = timezone.now().date()
+            keywords = KeywordOccurences.objects.get(date=date)
+            self.assertEqual(keywords._meta.get_field('date').verbose_name, 'Date')
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ search_history/tests/test_views.py
@@ -0,0 +1,60 @@
+
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from search_history.models import SearchHistory, KeywordOccurences
+import json
+
+
+User = get_user_model()
+class TestListHistory(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shifat',
+            email='shifat@gmail.com',
+            password='shifat123',
+        )
+        self.history = SearchHistory.objects.create(
+            user=self.user,
+            search_text='ukraine current situation',
+            search_time=timezone.now(),
+            search_result=json.dumps({'hello': 'world'})
+        )
+
+
+    def test_if_url_exist_in_desire_url(self):
+        response = self.client.get('/history/list/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_if_url_accessable_by_name(self):
+        response = self.client.get(reverse('search_history:list'))
+        self.assertEqual(response.status_code, 200)
+
+
+class TestHistoryDetail(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shifat',
+            email='shifat@gmail.com',
+            password='shifat123',
+        )
+        self.history = SearchHistory.objects.create(
+            user=self.user,
+            search_text='ukraine current situation',
+            search_time=timezone.now(),
+            search_result=json.dumps({'hello': 'world'})
+        )
+
+    def test_if_url_exist_in_desire_url(self):
+        response = self.client.get('/history/detail/{}/'.format(self.history.pk))
+        print('/history/detail/{}/'.format(self.history.pk))
+        self.assertEqual(response.status_code, 200)
+
+    def test_if_url_accessable_by_name(self):
+        response = self.client.get(reverse('search_history:detail', args=[self.history.pk]))
+        self.assertEqual(response.status_code, 200)
+
+
